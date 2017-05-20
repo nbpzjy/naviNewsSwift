d61a0b7bfde3c3bcf598a34a635e966a58be0c43
JIANYONG ZHOU <zjygzc@gmail.com>
2017-05-20 15:36:54 +0800

    Initial Commit

---
+++ NaviNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NaviNews
+//
+//  Created by YY's MacBook Pro on 2017/5/20.
+//  Copyright © 2017年 YY's MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
