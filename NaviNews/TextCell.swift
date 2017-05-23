d797c245702fcad9c8af4b8680bccb4124f7a1bb
YY's MacBook Pro <yymacbookpro@YYsmatoMacBook-Pro.local>
2017-05-23 12:31:38 +0800

    Completed pageMenu, tobe continue the details

---
+++ NaviNews/TextCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextCell.swift
+//  NaviNews
+//
+//  Created by YY's MacBook Pro on 2017/5/23.
+//  Copyright © 2017年 YY's MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class TextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- NaviNews/ViewController.swift
+++ NaviNews/ViewController.swift
@@ -15,15 +15,6 @@ class ViewController: UIViewController {
     var pageMenu : CAPSPageMenu!
     var controllers :[UIViewController] = []
     
-    
-
-    @IBOutlet weak var resultLable: UILabel!
-    @IBAction func testBtn(_ sender: UIButton) {
-        print("Button clicked")
-    }
-    @IBAction func cateBtnTap(_ sender: UIButton) {
-           }
-    
     func showMenu() {
         Category.request { (cates) in
             //            self.resultLable.text = "返回成功，类别数目： "+cates!.count.description
@@ -36,21 +27,21 @@ class ViewController: UIViewController {
             }
             let param : [CAPSPageMenuOption] = [
                 .menuItemSeparatorWidth(4.3),
-                .scrollMenuBackgroundColor(UIColor.white),
+                .scrollMenuBackgroundColor(UIColor.darkGray),
                 .viewBackgroundColor(UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)),
                 .bottomMenuHairlineColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 0.1)),
                 .selectionIndicatorColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
                 .menuMargin(20.0),
                 .menuHeight(40.0),
-                .selectedMenuItemLabelColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
-                .unselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
+                .selectedMenuItemLabelColor(UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)),
+                .unselectedMenuItemLabelColor(UIColor(red: 180/255.0, green: 180/255.0, blue: 200/255.0, alpha: 1.0)),
                 .menuItemFont(UIFont(name: "HelveticaNeue-Medium", size: 14.0)!),
-                .useMenuLikeSegmentedControl(true),
+                .useMenuLikeSegmentedControl(false),
                 .menuItemSeparatorRoundEdges(true),
                 .selectionIndicatorHeight(2.0),
                 .menuItemSeparatorPercentageHeight(0.1)
             ]
-            let fram = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            let fram = CGRect(x: 0, y: 20+44, width: self.view.frame.width, height: self.view.frame.height-20-44)
             
             self.pageMenu = CAPSPageMenu(viewControllers: self.controllers, frame: fram, pageMenuOptions: param)
             self.view.addSubview(self.pageMenu.view)
@@ -61,6 +52,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        showMenu()
     }
 
     override func didReceiveMemoryWarning() {
